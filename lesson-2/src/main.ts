ca9800fbc6623c6af58bd0d8deaa1815d9494846
Vitali Yanusheuski <butuk@users.noreply.github.com>
2023-07-31 13:05:24 +0200

    init: start secont lesson task

---
+++ lesson-2/src/main.ts
@@ -0,0 +1,19 @@
+function getJson(url, options = {}) {
+
+}
+
+{
+    res: true,
+    data: T,
+}
+
+{
+    res: false,
+    status: 'err',
+// }
+
+
+let a = getJson<TProduct[]>('https://faceprog.ru/reactcourseapi/products/index.php');
+//getJson<TProduct[]>('https://faceprog.ru/reactcourseapi/products/index.php?id=100')
+
+
